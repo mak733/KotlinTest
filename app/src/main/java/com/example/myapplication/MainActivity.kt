d067456a2e710535f384cb07c2d3e54416a651b1
TerryKosh <miss.fairy.terry@gmail.com>
2021-01-10 19:47:26 +0300

    Clock added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.myapplication
 
+//import android.R
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.view.ViewGroup
+import android.widget.*
+import android.widget.TimePicker.*
+//import android.widget.TimePicker.OnTimeChangedListener
+//import android.widget.DatePicker
+
+
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import org.w3c.dom.Text
+
+import java.util.Calendar
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,10 +28,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+//        var mInfoTextView = findViewById<TextView>(R.id.textView)
+//        var mTimePicker = findViewById<TimePicker>(R.id.timePicker)
+//
+//        val now = Calendar.getInstance()
+//
+//        mTimePicker.setHour(now[Calendar.HOUR_OF_DAY])
+//        mTimePicker.setMinute(now[Calendar.MINUTE])
+//
+//        mTimePicker.setOnTimeChangedListener(OnTimeChangedListener { view, hourOfDay, minute ->
+//            Toast.makeText(
+//                applicationContext, "onTimeChanged",
+//                Toast.LENGTH_SHORT
+//            ).show()
+//            mInfoTextView.setText(
+//                """
+//                    Часы: $hourOfDay
+//                    Минуты: $minute
+//                    """.trimIndent()
+//            )
+//        })
+//
+//        findViewById<Button>(R.id.button).setOnClickListener { view ->
+//            fun buttonOnClick(v: View?) {
+//                mInfoTextView.setText(
+//                    StringBuilder()
+//                        .append(mTimePicker.getHour()).append(".")
+//                        .append(mTimePicker.getMinute())
+//                )
+//            }
+//        }
+//
+//        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
+//        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
